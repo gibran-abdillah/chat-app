f5ee48e0d8a0b3509d1f24bb78bfd25b1a5749ed
Gibran Abdillah <unkownuser145@gmail.com>
2022-11-17 17:39:34 +0700

    add getMe view to urls

--- chat/urls.py
+++ chat/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('room/<str:room_code>', views.join_room, name='join_room'),
     path('create-room', views.create_room, name='create_room'),
     path('profile', views.profile, name='profile'),
+    path('getMe', views.getMe, name='get_me'),
     path('', views.index_chat, name='index_chat')
 ]
