d8fe424270c223e7df168fe92d63aa0312acc5f9
Gibran Abdillah <unkownuser145@gmail.com>
2022-11-18 20:25:39 +0700

    add: new profile url and others

--- public/api_urls.py
+++ public/api_urls.py
@@ -5,10 +5,10 @@ from . import views
 
 app_name = 'public'
 
-router = DefaultRouter()
-router.register('', views.UserViewSets, basename='user')
-
 urlpatterns = [
     path('register',views.register_api, name='api_register'),
-    path('login', views.login_api)
+    path('login', views.login_api),
+    path('getMe', views.getMe, name='get_me'),
+    path('my-room', views.my_room, name='my_room'),
+    path('profile', views.UserViewSets.as_view({'post':'update'}), name='profile_api')
 ]
