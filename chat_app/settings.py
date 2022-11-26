e086574b8be5c3acbaff635b725db4e1e3808b3c
Gibran Abdillah <unkownuser145@gmail.com>
2022-11-26 17:48:01 +0700

    add: 'BOT_DIR' to settings

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+BOT_DIR = os.path.join(BASE_DIR, 'bots')
 STATIC_URL = 'static/'
 STAITC_ROOT = 'static/'
 ASGI_APPLICATION = "chat_app.asgi.application"
