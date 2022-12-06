31db0fc32d2004d4b3d0f5aacc628e2d8f912920
Gibran Abdillah <unkownuser145@gmail.com>
2022-12-06 19:59:51 +0700

    another changes

---
+++ static/stat/chat/js/script.js
@@ -0,0 +1,161 @@
+const room = document.getElementById("room")
+const go_btn = document.getElementById("go")
+
+
+
+const websocket = new WebSocket(
+    'ws://' + window.location.host + '/ws/notif/' 
+)
+async function getMe() {
+    var response = await fetch('/api/user/getMe')
+    return response.json()
+}
+
+websocket.onopen = function() {
+    var response = getMe()
+    response.then((r) =>{
+
+        websocket.send(JSON.stringify(r))
+    })
+}
+
+function create_room_card(room_name, room_code) {
+
+    var div_sm = document.createElement("div")
+    div_sm.className = "col-sm-4 mb-2"
+
+    var card = document.createElement("div")
+    card.className = "shadow card bg-white rounded"
+
+    var card_body = document.createElement("div")
+    card_body.className = "card-body"
+
+    var card_title = document.createElement("h5")
+    card_title.className = "card-title"
+    card_title.innerHTML = room_name
+
+    var room_link = document.createElement("a")
+    room_link.className = "btn btn-dark"
+    room_link.href = '/chat/room/' + room_code
+    room_link.innerHTML = "Join"
+
+    card_body.appendChild(card_title)
+    card_body.appendChild(room_link)
+    card.appendChild(card_body)
+    div_sm.appendChild(card)
+
+    document.getElementById("card-row").appendChild(div_sm)
+    scroll_bottom()
+
+}
+
+
+async function check_room(room_code){
+    var response = await fetch(
+        '/api/chat/room/data/' + room_code + '/'
+    )
+    return response.status
+}
+
+async function get_room(custom_url=false) {
+    if (custom_url) {
+        url = custom_url
+    }else{
+        url = '/api/chat/room/data'
+    }
+    var response = await fetch(url)
+    return response.json()
+}
+
+function show_next_rooms() {
+    var next_button = document.getElementById("next-button")
+    var url_value = next_button.value
+    var room_response = get_room(custom_url=url_value)
+    show_room(room_response)
+    next_button.remove()
+
+
+}
+
+function show_next_button(next_url){
+    
+    var button = document.createElement("button")
+    button.className = "btn btn-outline-dark mt-3"
+    button.value = next_url
+    button.id = "next-button"
+    button.innerHTML = "Next"
+    button.setAttribute("onclick", "show_next_rooms()")
+    document.getElementById("button-position").appendChild(button)
+
+}
+
+
+
+function show_room(request) { 
+
+    request.then((r) =>{
+        if(r.next) {
+            show_next_button(r.next)
+            console.log(r.next)
+        }
+        if(r.results && r.results.length != 0) {
+            for(var i in r.results) {
+                var room_name = r.results[i].room_name
+                var room_code = r.results[i].room_code 
+                create_room_card(room_name, room_code)
+            }
+        }
+          
+    })
+
+}
+// 
+if(go_btn) {
+    var response_get_room = get_room(false)
+    show_room(response_get_room)
+    
+    go_btn.onclick = function() {
+        clean_message()
+        var room_code = document.getElementById("room_code")
+        if(room_code.value.length == 0) {
+            return 0
+        }
+        var response = check_room(room_code.value)
+        response.then((r) => {
+            if(r == 404) {
+                create_response("Room Not found!", failed=true)
+            }
+            else if(r == 200) {
+                window.location.href = '/chat/room/' + room_code.value
+            }
+            else {
+                create_response("Invalid response!", failed=true)
+            }
+        })
+        
+    }
+}
+function make_room(){
+    // clean_message()
+    var data = get_fields()
+    var room_status = document.getElementById("room_status")
+    data.is_public = room_status.value
+    var response = post_data('/api/chat/room/data/', data=data)
+    response.then((r) => {
+        if(r.status && r.room_code) {
+            create_response('Success, your room code is ' + r.room_code)
+            create_response("Quick Join <a href='/chat/room/" + r.room_code + "' target='_blank'>Touch me")
+        }
+    })
+    document.getElementById("room_code").value = ""
+
+}
+
+websocket.onmessage = function(e) {
+    var data_json = JSON.parse(e.data)
+    if(data_json.total_visitor) {
+        var visitor_id = document.getElementById("total_visitor")
+        visitor_id.innerHTML = data_json.total_visitor
+    }
+
+}
---
+++ static/stat/public/js/script.js
@@ -0,0 +1,315 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const room_row = document.getElementById("room-row")
+const message_id = document.getElementById("message")
+const header_token = getCookie('csrftoken')
+
+const register_url = '/api/user/register'
+const login_url = '/api/user/login'
+
+async function post_data(url, data) {
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type':'application/json',
+            'X-CsrfToken':header_token
+        }
+    })
+    return response.json()
+}
+function create_response(text, failed=false) {
+    var div = document.createElement("div")
+    if(failed){
+        div.className = "alert alert-danger"
+        div.innerHTML = "<strong>Failed!</strong> " + text 
+    }else{
+        div.className = "alert alert-success"
+        div.innerHTML = "<strong>Success!</strong> " + text 
+    }
+    
+
+    message_id.appendChild(div)
+
+}
+
+function get_fields(){
+    let data = {};
+    var input = document.querySelectorAll('input')
+    for(i in input) {
+        if(input[i].name && input[i].value) {
+            data[input[i].name] = input[i].value
+        }
+    }
+    return data 
+}
+
+function login_user() {
+    let data = get_fields()
+    post_data(login_url, data).then((response) => {
+        if(response.success) {
+            create_response('Logged in! Redirecting...')
+            window.location.pathname = '/chat/'
+        }else{
+            var response = response ? response.message : response.detail
+            create_response(response, failed=true)
+        }
+    }
+    )
+    clean_message()
+
+}
+function clean_inputs() { 
+    var data = document.querySelectorAll("input")
+    for(i in data){
+        if(data[i].type !== 'hidden') {
+            data[i].value = ''
+        }
+    }
+}
+function scroll_bottom() {
+    // Scroll to end of body 
+    var row_class = document.querySelector(".row")
+    window.scrollTo(0, row_class.scrollHeight + 1000)
+}
+function register_user() {
+    clean_message()
+    let data = get_fields()
+    post_data(register_url, data).then((response) => {
+        if(response.success) { 
+            create_response('Registered, you can login now')
+            clean_inputs()
+        }else{
+            for(i in response) {
+                var raw_response = response[i][0]
+                if(raw_response == 'This field is required.') {
+                    text = "Missing Form!"
+                }else{
+                    text = raw_response
+                }
+
+                create_response(text, failed=true)
+            }
+        }
+
+
+        }
+
+    )
+
+}
+function clean_message() {
+    document.getElementById("message").innerHTML = ''
+}
+function change_password() {
+    clean_message()
+    var data = {}
+    var input_fields = document.querySelectorAll("input")
+    for(var i in input_fields) {
+        if(input_fields[i].type == 'password' && input_fields[i].value.length != 0) {
+            data[input_fields[i].name] = input_fields[i].value
+        }
+    }
+    var response = post_data('/api/user/change-password', data=data)
+    response.then((r) => {
+        if(r.success) {
+            create_response("Password Changed!", failed=false)
+        }else{
+            for(i in r) {
+                create_response(r[i][0].message, failed=true)
+            }
+        }
+    })
+}
+function update_user() {
+    clean_message()
+    var data = get_fields()
+    var z = post_data('/api/user/profile', data)
+    z.then((r) => {
+        if(r.success) {
+            create_response("Changed!")
+        }else{
+            if(r.length != 0) {
+                for(var i in r) {
+                    //console.log(r[i][0].message)
+                    create_response(r[i][0].message, failed=true)
+                }
+            }
+        }
+    })
+}
+function create_my_room_card(room_name, room_code) {
+    var element = `
+    <div class="col-md-3 mt-3">
+                <div class="card shadow rounded">
+                    <div class="card-body">
+                        <h5 class="card-title mb-3">${room_name}
+                            <i class="ml-2 fa fa-files-o" onclick="copy_room_url(this)" room_code="${room_code}"></i>
+                        </h5>
+                        <a href="/chat/room/${room_code}" class="btn btn-dark"><i class="fa fa-paper-plane-o fa-2x"></i></a>
+                        <a href='/profile/room/${room_code}/setting' class="btn btn-dark"><i class="fa fa-cog fa-2x"></i></a> 
+                        <button class="btn btn-primary" room_code="${room_code}" onclick="refresh_code(this)"><i class="fa fa-refresh fa-2x"></i></button>
+                        <button class="btn btn-danger" room_code="${room_code}" onclick="delete_room(this)"><i class="fa fa-trash-o fa-2x"></i></button>
+                        
+                    </div>
+                </div>
+    `
+    room_row.innerHTML += element
+    scroll_bottom()
+}
+function copy_room_url(element){
+    var room_code = element.getAttribute("room_code")
+    console.log(room_code)
+    var room_url = `${window.location.host}/chat/room/${room_code}`
+    //element.select.setSelectionRange(0, 9999)
+    navigator.clipboard.writeText(room_url)
+    alert("Copied!")
+}
+async function request_delete_room(room_code) {
+    var response = await fetch('/api/user/delete-room/' + room_code)
+    return response.json()
+}
+
+function delete_room(element) {
+    var room_code = element.getAttribute("room_code")
+    var response = request_delete_room(room_code)
+    response.then((r) =>{
+        if(r.success) {
+            create_response('Deleted', failed=false)
+            element.parentNode.parentNode.parentNode.parentNode.removeChild(element.parentNode.parentNode.parentNode)
+        }else if(r.detail){
+            create_response('Room not found', failed=true)
+        }else if(!r.success) {
+            create_response('invalid room', failed=true)
+        }
+    })
+    // remove element from room card
+    
+
+}
+function refresh_code(element) {
+    // Function to Refresh Room Code
+    var room_code = element.getAttribute("room_code")
+    var response = get_my_room("/api/user/refresh-code/" + room_code)
+    response.then((r) =>{
+        if(r.detail) {
+            create_response("Invalid room (404)", failed=true)
+        }
+        if(r.success && r.room_code) {
+            create_response("New room code generated!", failed=false)
+            var child_nodes = element.parentNode.children
+            for(i in child_nodes) {
+                if(child_nodes[i].tagName == 'BUTTON') {
+                    child_nodes[i].setAttribute("room_code", r.room_code)
+                }
+                if(child_nodes[i].tagName == 'A') {
+                    child_nodes[i].href = "/chat/room/" + r.room_code
+                }
+                if(child_nodes[i].tagName == 'H5') {
+                    child_nodes[i].children[0].setAttribute("room_code", r.room_code)
+                }
+            }
+        }else{
+            create_response("Invalid response", failed=true)
+        }
+    })
+    
+
+}
+
+function create_next(url) {
+    var next_position = document.getElementById("next-position")
+    var element = `
+    <button type="submit" class="btn btn-outline-dark" value="${url}" onclick="next_my_room(this)">Next</button>
+    `
+    next_position.innerHTML = element
+}
+function next_my_room(element) {
+    var next_url = element.value 
+    var response = get_my_room(next_url)
+    response.then((r) => {
+        show_room_card(r)
+    })
+
+}
+async function get_my_room(custom_url=false) {
+    if(custom_url) {
+        url = custom_url
+    }else{
+        url = '/api/user/my-room'
+    }
+    var response = await fetch(url)
+    return response.json()
+}
+
+function show_room_card(r) {
+    if(r.next) {
+        create_next(r.next)
+    }else{
+        document.getElementById("next-position").innerHTML = ""
+    }
+    if(r.results.length !== 0){
+        for(i in r.results) {
+            create_my_room_card(r.results[i].room_name, r.results[i].room_code)
+        }
+    }
+}
+function get_selected() {
+    var select = document.querySelectorAll("option:checked")
+    var result = {}
+    for(i in select) {
+        var parent_node = select[i].parentNode
+        if(parent_node) {
+            if(!result[parent_node.name]) { 
+
+                result[parent_node.name] = []
+            }
+            if(parent_node.hasAttribute('multiple')){
+                result[parent_node.name].push(select[i].value)
+            }else{
+                result[parent_node.name] = select[i].value
+            }
+        }
+    }
+    console.log(result)
+    return result 
+}
+function room_setting() {
+    var data = get_fields()
+    var selected = get_selected()
+    
+    data['is_public'] = selected.is_public
+    data['blocked_users'] = selected.blocked_users
+    data['active_bots'] = selected.active_bots
+
+    var response = post_data('/api/user/room-setting', data)
+
+    response.then((y)=>{
+        if(y.success) {
+            create_response("updated")
+        }
+    })
+    
+}
+
+
+if(room_row) {
+    var response = get_my_room()   
+    response.then((r)=> {
+        show_room_card(r)
+    })
+}
